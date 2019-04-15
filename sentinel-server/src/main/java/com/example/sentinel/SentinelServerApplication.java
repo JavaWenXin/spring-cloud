73c4f908f3a7e625c6582455d270fc808fe8603c
WenXin <xin.wen3@56qq.com>
2019-04-15 10:47:29 +0800

    ribbon

---
+++ sentinel-server/src/main/java/com/example/sentinel/SentinelServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentinel;
+
+import org.springframework.boot.loader.JarLauncher;
+
+//@SpringBootApplication
+public class SentinelServerApplication {
+
+    public static void main(String[] args) throws Exception {
+        JarLauncher.main(args);
+        //SpringApplication.run(SentinelServerApplication.class, args);
+    }
+
+}
